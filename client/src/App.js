aad99afa4fbcf065828d3827f554ed5b4ffb4daf
asibat <asibat@wiser.com>
2019-06-06 22:48:47 +0300

    Create client side app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import ReposMangerView from './ReposManger'
+
+function App() {
+  return (
+    <div className="App">
+      <ReposMangerView />
+    </div>
+  )
+}
+
+export default App
